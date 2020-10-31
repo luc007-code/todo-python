5f2d0a71a752cde403790b9d64e603d2ecaf1d80
luc007-code <73670652+luc007-code@users.noreply.github.com>
2020-10-31 12:32:44 +0000

    added main

---
+++ main.py
@@ -0,0 +1,175 @@
+import platform
+import os
+import shutil
+import time
+import sys, getopt
+file="tasks.db"
+## check the system in use to clean the screen
+OS = platform.system()
+
+def CleanScreen():
+    ## clean the screen
+    if OS == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def MakeHeaders(Msg, Char):
+    import shutil
+    #get terminal width
+    #terminal = os.get_terminal_size()
+    terminal = shutil.get_terminal_size()
+    width = terminal.columns
+    Msg_len = len(Msg)
+    #Char = "*"
+    Total_Char = width - (Msg_len + 2)
+    return str(Char * int(Total_Char/2) + " " + Msg + " " + Char * int(Total_Char/2))
+
+def MakeLines(Char):
+    terminal = shutil.get_terminal_size()
+    width = terminal.columns
+    return str(Char * int(width))
+def MakeMenuHeader(Menu, char):
+    CleanScreen()
+    print(MakeHeaders(str(Menu),str(char)))
+    print(MakeLines(str(char)))
+def MainMenu():
+    MakeMenuHeader("Get things DONE!","*")
+    print("1- Add a task")
+    print("2- Delete a task")
+    print("3- List task")
+    print("q- Quit")
+    print(MakeLines("-"))
+    opt=input("Choose an option: ")
+    return opt 
+
+def ListTasks(CMD):
+    f = open(file,"r")
+    j = f.read()
+    f.close()
+    FileSplitted = j.split('\n')
+    if (len(FileSplitted)-1) == 0:
+        print("Nice! you have no tasks to do!")
+    else:
+        for x in range(len(FileSplitted)-1):
+            print(str(x) + " - " + FileSplitted[x])
+        if not CMD == True:
+            input("Hit enter to exit to main menu")
+#    MakeMenuHeader("List tasks","-")
+#    try:
+#        db = open(file,"r")
+#        tasks = db.read()
+#        print(tasks)
+#    except FileNotFoundError:
+#        print("No file found, check for "+ file)
+
+#def WriteTask(task,date):
+def WriteTask(task):
+
+    try:
+        db = open(file,"a+")
+        task = task + "\n"
+        db.write(task)
+        print("Saved!")
+        db.close
+    except (IOError, FileNotFoundError) as e:
+        print("Failed to write to the file "+ file)
+
+def AddTask():
+    MakeMenuHeader("Add tasks","-")
+    task = str(input("Type a task to be added to the list: "))
+    date = str(input("Type a deadline date mm-dd-yy: "))
+    #print("Task: {} Until: {}.".format(task,date) )
+    print("Task: {}.".format(task) )
+
+    ret = str(input("Is this task right? (y/n): "))
+    if ret in ('y','Y','yes','Yes','YES'):
+        #WriteTask(task,date)
+        WriteTask(task)
+
+
+def RemoveTasks():
+    MakeMenuHeader("Remove a task","-")
+    f = open(file,"r")
+    j = f.read()
+    f.close()
+    FileSplitted = j.split('\n')
+    if (len(FileSplitted)-1) == 0:
+        print("Nice! you have no tasks to do!")
+    else:
+        for x in range(len(FileSplitted)-1):
+            print(str(x) + " - " + FileSplitted[x])
+        ret=int(input("Wich one do you want to remove? [numbers]: "))
+        if ret > (len(FileSplitted)-2):
+            print("Choose a number from 0 to "+ str(len(FileSplitted)-2))
+        else:
+            try:
+                # cleanup the file
+                with open(file, 'w'): pass
+                FileSplitted.pop(ret)
+                #print(FileSplitted)
+                f=open(file, 'w')
+                for x in range(len(FileSplitted)-1):
+                    f.write(FileSplitted[x] + "\n")
+                f.close()
+                print("task "+ str(ret) +" removed")
+            except ValueError:
+                print("Only numbers are accepted")
+
+def ScreenManual():
+    while True:
+       opt=MainMenu()
+       if opt == "1":
+           print("Create function add task")
+           AddTask()
+       elif opt == "2":
+           print("Create function Delete task")
+           RemoveTasks()
+       elif opt == "3":
+           print("create function list tasks")
+           ListTasks(False)
+       elif opt == "q":
+           quit()
+           break
+       else:
+           print("Invalid Option, try again")
+   
+       time.sleep(1)
+def ShowHelp():
+       MakeMenuHeader("Help!","*")
+       print("The following options are available in the command line mode: \n -t or --task  : adds a task using the argument as a name \
+                                                   \n -d or --date  : adds dead line for this added task dateformat MM-DD-YY\
+                                                   \n -l or --list  : lists all tasks\
+                                                   \n -h or --help  : show this help\
+                                                   \n\n\
+             Examples:\n\
+             Add a task:\n\
+             " + sys.argv[0] + " -t Task1 -d 07-30-18\n\n\
+             IMPORTANT:\n\
+             if you whant to use this program in a screen mode, just use it without arguments! ;)")
+def WithArgs():
+    try:
+       opts, args = getopt.getopt(sys.argv[1:],"d:hlt:",["task=","date=","help","list"])
+    except getopt.GetoptError:
+        ShowHelp()
+    date, task = '',''
+    for opt, arg in opts:
+       if opt in ('-t', 'task'):
+           task = arg
+       if opt in ('-d', 'date'):
+           date = arg
+       if opt in ('-h', 'help'):
+           ShowHelp()
+           quit()
+       if opt in ('-l', 'list'):
+           ListTasks(True)
+           quit()
+    print(task,date)
+    WriteTask(task)
+
+if len(sys.argv[1:]) > 0:
+    WithArgs()
+else:
+    print("manual")
+    ScreenManual()
